62ac018ff37d3060d0fab2ab3259648685dc8d59
aureliehennebel <aurelie.hennebel@etu.unilim.fr>
2017-03-10 11:31:23 +0100

    modif fonctionnelle reseau

--- DW04/DW04/www/scripts/script.js
+++ DW04/DW04/www/scripts/script.js
@@ -17,7 +17,7 @@
         } else {
             // Envoi de la requête HTTP en mode asynchrone
             $.ajax({
-                url: 'http://localhost/dw2/traitement.php', // Le nom du fichier indiqué dans le formulaire
+                url: 'http://xjapanfan.com/unilimcalendar/traitement.php', // Le nom du fichier indiqué dans le formulaire
                 type: 'POST', // La méthode indiquée dans le formulaire (get ou post)
                 data: $this.serialize(), // Je sérialise les données (j'envoie toutes les valeurs présentes dans le formulaire)
                 dataType: 'json', //format de sortie json
